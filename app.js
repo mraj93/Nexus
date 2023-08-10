833e0c2202986fe03eb6ced5fce4fb478bbe5da4
Mukesh-imac <mrajbhar009@gmail.com>
2023-08-10 17:22:30 +0530

    configuration changed

--- app.js
+++ app.js
@@ -64,6 +64,7 @@ const mintERC721 = async (req, res) => {
                 to: ContractAddress,
                 data: data,
             });
+            
             const bufferPercentage = 10;
             const bufferedGasLimit = gasLimit * BigInt(100 + bufferPercentage) / BigInt(100);
             const txn = await web3.eth.accounts.signTransaction({
